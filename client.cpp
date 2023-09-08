92c3d043f5d5d6031a7c608e7bbfb00b0840f61c
MahdiGMK <mbahramiand@gmail.com>
2023-09-08 21:55:29 +0330

    exposed port

--- client.cpp
+++ client.cpp
@@ -6,17 +6,18 @@
 #include <chrono>
 #include "resources/GameHandler.h"
 #include <mutex>
+using namespace std;
 
 
 
-
-const int PORT = 12345;
+// const int PORT = 12345;
 const std::string HOST = "127.0.0.1";
 
 extern Game game;
 
 
-int main() {
+int main(int argc , char** argv) {
+	const int PORT = stoi(argv[1]);
 	
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	GameHandler handeler(HOST,PORT);
